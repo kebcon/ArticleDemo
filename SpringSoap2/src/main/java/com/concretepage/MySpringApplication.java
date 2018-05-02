8ffc3ac2c53955586de95d1f9babfcdf0faf14f5
Kamau <kebcon94@gmail.com>
2018-05-02 11:59:30 -0400

    Updating Connection..Causing Error...

--- SpringSoap2/src/main/java/com/concretepage/MySpringApplication.java
+++ SpringSoap2/src/main/java/com/concretepage/MySpringApplication.java
@@ -1,9 +1,12 @@
 package com.concretepage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class MySpringApplication {  
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringApplication.class, args);
