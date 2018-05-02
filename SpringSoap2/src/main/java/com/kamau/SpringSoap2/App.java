de6f48d3a8152bcf64f91f5811eb76a1f643cfc1
Kamau <kebcon94@gmail.com>
2018-05-02 10:02:04 -0400

    first commit in Eclispe...

---
+++ SpringSoap2/src/main/java/com/kamau/SpringSoap2/App.java
@@ -0,0 +1,13 @@
+package com.kamau.SpringSoap2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
