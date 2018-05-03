3063bf173886c988c341c4ea67c0157bf783edd0
Kamau <kebcon94@gmail.com>
2018-05-02 23:20:39 -0400

    Correct Properties...

--- SpringSoap2/src/main/java/com/concretepage/MySpringApplication.java
+++ SpringSoap2/src/main/java/com/concretepage/MySpringApplication.java
@@ -1,13 +1,11 @@
 package com.concretepage;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-
 public class MySpringApplication {  
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringApplication.class, args);
         }       
